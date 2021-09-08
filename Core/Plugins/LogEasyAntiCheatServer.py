8dc39c40360478f368afa5a38aca729f69269c3c
Chenjian Shi <chenjianshi47@gmail.com>
2021-09-08 19:00:08 +0200

    Update LogEasyAntiCheatServer.py

--- Core/Plugins/LogEasyAntiCheatServer.py
+++ Core/Plugins/LogEasyAntiCheatServer.py
@@ -6,8 +6,8 @@ import datetime
 def LogEasyAntiCheatServer(data_str):
     matchObj = re.search(r"[([0-9.:-]+]\[([ 0-9]*)]LogEasyAntiCheatServer: .*Client: (.*) PlayerGUID: (.*) "
                          r"PlayerIP: (.*) OwnerGUID: (.*) PlayerName: .", data_str, re.M | re.I)
-    date_time = datetime.datetime.strptime(matchObj.group(0).split("[")[1].split(']')[0], "%Y.%m.%D-%H.%M.%S:%f")
-    date_time_n = (date_time + datetime.timedelta(hours=8)).strftime("%Y.%m.%D-%H.%M.%S:%f")
+    date_time = datetime.datetime.strptime(matchObj.group(0).split("[")[1].split(']')[0], "%Y.%m.%d-%H.%M.%S:%f")
+    date_time_n = (date_time + datetime.timedelta(hours=8)).strftime("%Y.%m.%d-%H.%M.%S:%f")
     date = date_time_n.split("-")[0]
     time = date_time_n.split("-")[1]
     player_64id = matchObj.group(3)

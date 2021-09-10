646526b384103447b81d6b1715d6394666338deb
Chenjian Shi <chenjianshi47@gmail.com>
2021-09-10 22:16:34 +0200

    Fix gitignore and remove Rcon.py

--- Core/Base/Rcon.py
+++
@@ -1,9 +0,0 @@
-import Core.Plugins.rcon_pyscrds as rcon
-from config import server_rcon
-
-
-def rcon_cmd(threadID):
-    rcon.RconConnection(server=server_rcon[threadID]['Server_IP'],port=server_rcon[threadID]['RCON_Port'],
-                        password=server_rcon[threadID]['Server_pw'])
-
-    pass
